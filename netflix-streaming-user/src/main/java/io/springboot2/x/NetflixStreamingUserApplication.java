c4aab80ae541fb44472aaff88c26a16b848ba557
giri <giri@LAPTOP-1Q3S0RD2>
2021-03-24 20:36:40 +0530

    3rd commit...

---
+++ netflix-streaming-user/src/main/java/io/springboot2/x/NetflixStreamingUserApplication.java
@@ -0,0 +1,33 @@
+package io.springboot2.x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class NetflixStreamingUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixStreamingUserApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();  
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate2(){
+		return new RestTemplate();
+	}
+	
+	
+	
+	
+	
+	
+}
